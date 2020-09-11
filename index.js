c097dfa8576155ef89f59653d8dfefca138bca48
Александр Долганов <dalex3734@yandex.ru>
2020-09-11 23:47:33 +0300

    fixed errors, add keyboard control

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let CENTER = [WIDTH / 2, HEIGHT / 2] // 0.9 - размер области с п�
 console.log(WIDTH, HEIGHT)
 const T_coeff = 0.05 //Регулировка скорости
 const G_coeff = 10000
-let outputCoordinates = document.getElementsByClassName("coordinates")[0]
+let outputCoordinates = document.getElementsByClassName("coordinates")[0] // Вывод координат в div
 let paused = false
 
 // Настраиваем canvas
@@ -19,27 +19,29 @@ window.onload = function() {
 
 // Массив с планетами
 var planets = [];
+var futurePlanet = []
 
 // Тип данных для планеты
 function Planet(x, y, v, angle, radius, mass, color) {
     this.x = x;
 	this.y = y;
-	this.vx = v * Math.cos(Math.PI/180*angle)
-	this.vy = v * Math.sin(Math.PI/180*angle)
+	this.angle = angle + 180;
+	this.vx = v * Math.cos(Math.PI/180*this.angle);
+	this.vy = - v * Math.sin(Math.PI/180*this.angle);
 	this.radius = radius;
 	this.mass = mass
     this.color = color;
 }
 
 // Объявляем первые планеты (x, y, v, angle, radius, mass, color)
-var sun = new Planet(CENTER[0], CENTER[1], 0, 0, 10, 100, "yellow")
+var sun = new Planet(CENTER[0], CENTER[1], 0, 0, 8, 100, "yellow")
 planets.push(sun)
-var mercury = new Planet(CENTER[0] + 100, CENTER[1], 100, 90, 5, 1, "red")
+var mercury = new Planet(CENTER[0] + 100, CENTER[1], 90, 90, 2, 1, "red")
 planets.push(mercury)
-var venus = new Planet(CENTER[0] - 300, CENTER[1], 50, 270, 5, 2, "brown")
-planets.push(venus)
+var earth = new Planet(CENTER[0] -200, CENTER[1], 75, 270, 4, 2, "blue")
+planets.push(earth)
 
-function addPlanet() {
+function addPlanet(array = planets) {
 	var planet = new Planet(
 		CENTER[0] + Number(document.getElementsByName("x")[0].value),
 		CENTER[1] - Number(document.getElementsByName("y")[0].value),
@@ -49,10 +51,11 @@ function addPlanet() {
 		Number(document.getElementsByName("mass")[0].value),
 		document.getElementsByName("color")[0].value,
 		)
-	planets.push(planet)
+	array.push(planet)
 	if (paused) {
 		drawFrame()
 	}
+	if (array != futurePlanet) { futurePlanet = [] }
 }
 
 function deletePlanet(index) {
@@ -77,12 +80,29 @@ function drawFrame() {
 		let planet = planets[i]
 		context.arc(planet.x, planet.y, planet.radius, 0, 7);
 		context.fillStyle = planet.color
+		context.shadowColor = '#ffffff'
+        context.shadowBlur = 30 //Свечение
 		context.fill();
 		// Вывести координаты на экран
 		let v = Math.sqrt(Math.pow(planet.vx, 2) + Math.pow(planet.vy, 2))
 		outputCoordinates.innerHTML += `${i}. x = ${Math.round(planet.x) - CENTER[0]},
 			y = ${CENTER[1] - Math.round(planet.y)}, V = ${Math.round(v)}, mass = ${planet.mass}.
 			<span onclick="deletePlanet(${i})">delete</span> <br>`	
+	}
+	if ( futurePlanet.length > 0 ) {
+		let planet = futurePlanet[0]
+		context.arc(planet.x, planet.y, planet.radius, 0, 7); // Рисуем будущую планету
+		context.fillStyle = 'rgba(225,225,225,0.5)'
+		context.shadowColor = '#ffffff'
+		context.shadowBlur = 30 //Свечение
+		context.fill();
+		context.beginPath();
+		drawArrow(planet.x, planet.y, 
+			planet.x + 40 * Math.cos(Math.PI/180*planet.angle),
+			planet.y - 40 * Math.sin(Math.PI/180*planet.angle));
+			debugger
+		context.strokeStyle = 'rgba(225,225,225,0.5)'
+		context.stroke();
 	}
 	if(!paused) {
 		requestAnimationFrame(drawFrame);
@@ -133,6 +153,73 @@ function togglePaused() {
 	if (!paused) drawFrame()
 }
 
+// Расчёт для отрисовки стрелки
+function drawArrow(fromx, fromy, tox, toy) {
+	let headlen = 10; // length of head in pixels
+	let dx = tox - fromx;
+	let dy = toy - fromy;
+	let angle = Math.atan2(dy, dx);
+	context.moveTo(fromx, fromy);
+	context.lineTo(tox, toy);
+	context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
+	context.moveTo(tox, toy);
+	context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+}
+
+// Движения по экрану
+function moveUp() {
+	for (let i = 0; i < planets.length; i++) {
+		planets[i].y += 10
+	}
+}
+function moveDown() {
+	for (let i = 0; i < planets.length; i++) {
+		planets[i].y -= 10
+	}
+}
+function moveLeft() {
+	for (let i = 0; i < planets.length; i++) {
+		planets[i].x += 10
+	}
+}
+function moveRight() {
+	for (let i = 0; i < planets.length; i++) {
+		planets[i].x -= 10
+	}
+}
+
+// Управление клавишами
+addEventListener("keydown", function(event) {
+    if (event.keyCode === 38) {moveUp()}
+    if (event.keyCode === 40) {moveDown()}
+    if (event.keyCode === 37) {moveLeft()}
+    if (event.keyCode === 39) {moveRight()}
+    if (event.keyCode === 32) {togglePaused()}
+});
+
+// Добавляем "будущую" планету
+addEventListener("click", function (event) {
+	if (event.clientX < WIDTH * 0.28 && event.clientY > HEIGHT * 0.72) {
+		return false
+	} else if (futurePlanet.length == 0) {
+		sliderX.value = event.pageX - CENTER[0]
+		outputX.innerHTML = sliderX.value; 
+		sliderY.value = CENTER[1] - event.pageY
+		outputY.innerHTML = sliderY.value;
+		addPlanet(futurePlanet)
+	} else if (futurePlanet.length == 1) {
+		let tox = event.pageX
+		let toy = event.pageY
+		let dx = tox - futurePlanet[0].x;
+		let dy = futurePlanet[0].y - toy;
+		let angle = Math.atan2(dy, dx);
+		if (angle < 0) {angle += 2 * Math.PI}
+		sliderAngle.value = (180*angle/Math.PI); 
+		outputAngle.innerHTML = sliderAngle.value;
+		futurePlanet[0].angle = (180*angle/Math.PI)
+	}
+})
+
 // Управление ползунками
 var sliderX = document.getElementsByName("x")[0]
 var outputX = document.getElementById("x");
@@ -175,3 +262,4 @@ outputMass.innerHTML = sliderMass.value;
 sliderMass.oninput = function() {
     outputMass.innerHTML = this.value;
 }
+
